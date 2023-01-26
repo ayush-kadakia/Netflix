88dacc5ec93044f70fcee626699c93a091d5487c
Ayush Kadakia <ayush.kadakia@live.com>
2023-01-26 03:22:27 -0500

    API Uses Models

--- Netflix/Models/Movie.swift
+++ Netflix/Models/Movie.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct TrendingMoviesResponse: Codable {
+    let results: [Movie]
+}
+
+struct Movie: Codable {
+    let id: Int
+    let media_type: String?
+    let original_language: String?
+    let original_title: String?
+    let overview: String?
+    let popularity: Float?
+    let poster_path: String?
+    let release_date: String?
+    let title: String?
+    let vote_average: Float?
+    let vote_count: Int?
+}
+
+
