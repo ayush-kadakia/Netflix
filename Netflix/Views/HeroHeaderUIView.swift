8d9c52068db3688c964d70b815b2f0c63f810960
Ayush Kadakia <ayush.kadakia@live.com>
2023-01-26 02:30:28 -0500

    Home View + Tab Bar + Cells

---
+++ Netflix/Views/HeroHeaderUIView.swift
@@ -0,0 +1,20 @@
+//
+//  HeroHeaderUIView.swift
+//  Netflix
+//
+//  Created by Ayush Kadakia on 1/26/23.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
